e62b07776db8e00f3d83af90e13339a2836c90fe
jason banfield <developer@dmxify.com>
2019-11-29 23:39:30 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * MidiDevice class and utility module.
+ * @module midi-device
+ * @version 0.1.0
+ * @author Jason Banfield <developer@dmxify.com>
+ * @license MIT
+ */
+
+/**
+ * @class MidiDevice
+ * @property {number} id - MidiDevice id
+ * @property {string} name - MidiDevice name
+ * @property {string} manufacturer - MidiDevice manufacturer
+ * @property {string} imagePath
+ */
+const MidiDevice = class {
+  constructor({
+    id = null,
+    name = 'New Midi Device',
+    manufacturer = 'Generic',
+    imagePath = ''
+  }) {
+    this._id = id;
+    this._name = name;
+    this._manufacturer = manufacturer;
+    this._imagePath = imagePath;
+  }
+
+  get id() {
+    return this._id;
+  }
+  set id(val) {
+    this._id = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(val) {
+    this._name = val;
+  }
+
+  get manufacturer() {
+    return this._manufacturer;
+  }
+  set manufacturer(val) {
+    this._manufacturer = val;
+  }
+
+  get imagePath() {
+    return this._imagePath;
+  }
+  set imagePath(val) {
+    this._imagePath = val;
+  }
+}
+
+module.exports = MidiDevice;

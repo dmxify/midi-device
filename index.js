ab141353b1f439fe7132ff20efdbdeee28e581dc
jason banfield <developer@dmxify.com>
2019-12-22 18:55:09 +0200

    - unescaping html period from saved data (workaround) - internal MidiDeviceOptions class

--- index.js
+++ index.js
@@ -14,13 +14,47 @@
  * @property {array} midiDeviceControls - Array of MidiDeviceControl objects configured for this MidiDevice
  */
 
+
+const unescapePeriod = function(str) {
+  str.replace('&period;', '.');
+  return str;
+}
+
+
+const MidiDeviceOptions = class {
+  constructor({
+    imagePath = "",
+    enabled = true
+  } = {
+    imagePath: "",
+    enabled: true
+  }) {
+    this.imagePath = imagePath;
+    this.enabled = !!enabled;
+  }
+
+  getEnabled() {
+    return this.enabled;
+  }
+  setEnabled(val) {
+    this.enabled = !!val;
+  }
+
+  getImagePath() {
+    return this.imagePath;
+  }
+  setImagePath(val) {
+    this.imagePath = val
+  }
+}
+
 const MidiDevice = class {
   constructor({
     id = null,
     name = 'New Midi Device',
     manufacturer = 'Generic',
     midiDeviceControls = [],
-    ui = {
+    options = {
       "imagePath": "",
       "enabled": true
     }
@@ -29,17 +63,16 @@ const MidiDevice = class {
     name: 'New Midi Device',
     manufacturer: 'Generic',
     midiDeviceControls: [],
-    ui: {
+    options: {
       "imagePath": "",
       "enabled": true
     }
   }) {
     this._id = id;
-    this._name = name;
-    this._manufacturer = manufacturer;
+    this._name = unescapePeriod(name);
+    this._manufacturer = unescapePeriod(manufacturer);
     this._midiDeviceControls = midiDeviceControls;
-    this._ui = ui;
-    this.hasControlWithBindingOf = this.hasControlWithBindingOf.bind(this)
+    this._options = new MidiDeviceOptions(options);
   }
 
   get id() {
@@ -53,21 +86,14 @@ const MidiDevice = class {
     return this._name;
   }
   set name(val) {
-    this._name = val;
+    this._name = unescapePeriod(val);
   }
 
   get manufacturer() {
     return this._manufacturer;
   }
   set manufacturer(val) {
-    this._manufacturer = val;
-  }
-
-  get imagePath() {
-    return this._imagePath;
-  }
-  set imagePath(val) {
-    this._imagePath = val;
+    this._manufacturer = unescapePeriod(val);
   }
 
   get midiDeviceControls() {
@@ -77,12 +103,12 @@ const MidiDevice = class {
     this._midiDeviceControls = val;
   }
 
-  get ui() {
-    return this._ui;
+  get options() {
+    return this._options;
   }
 
-  set ui(val) {
-    this._ui = val;
+  set options(val) {
+    this._options = val;
   }
 
   /**
@@ -93,6 +119,16 @@ const MidiDevice = class {
   addMidiDeviceControl(midiDeviceControl) {
     this._midiDeviceControls.push(midiDeviceControl);
   }
+
+  deleteMidiDeviceControl(id) {
+    for (var i = 0; i < this._midiDeviceControls.length; i++) {
+      if (this._midiDeviceControls[i]._id == id) {
+        this._midiDeviceControls.splice(i, 1);
+      }
+    }
+  }
+
+
   nextAvailableControlId() {
     return Math.max(...this._midiDeviceControls.map(o => o.id), 0) + 1;
   }
@@ -100,39 +136,6 @@ const MidiDevice = class {
   numOfControlType(type) {
     return this._midiDeviceControls.filter((o) => o.type === type).length;
   }
-
-  /**
-   * Determine if this MidiDevice has a MidiDeviceControl with a specific MidiMessage binding
-   * @param {object} MidiMessage - MidiMessage.values array to search for. e.g. "[10,45,127]"
-   * @returns {boolean} - true if control exists, false if it doesn't exist
-   */
-  hasControlWithBindingOf(midiMessageData) {
-    let type;
-    // for each MidiDeviceControl in this MidiDevice
-    for (let control of this._midiDeviceControls) {
-      // iterate through midiMessageBindings. (format: [channel,note,value])
-      for (let binding of control.midiMessageBindings) {
-        switch (control.type) {
-          case 'BUTTON':
-            // compare channel, note & value
-            if (midiMessageData[0] == binding[0] && midiMessageData[1] == binding[1] && midiMessageData[2] == binding[2]) {
-              return true;
-            }
-            break;
-          case 'FADER':
-            // compare channel and note
-            if (midiMessageData[0] == binding[0] && midiMessageData[1] == binding[1]) {
-              return true;
-            }
-            break;
-          default:
-            break;
-        } // end switch
-      } // end for
-    } // end for
-    return false;
-  } // end hasControlWithBinding method
-
 
   static hasControlWithBindingsOf(midiDevice, controlType, midiMessageData) {
     // for each MidiDeviceControl in this MidiDevice
@@ -142,7 +145,7 @@ const MidiDevice = class {
     for (let control of midiDevice.midiDeviceControls) {
       // iterate through midiMessageBindings. (format: [channel,note,value])
       for (let binding of control._midiMessageBindings) {
-        switch (control.type) {
+        switch (control._type) {
           case 'BUTTON':
             // compare channel, note & value
             if (midiMessageData[0] == binding[0] && midiMessageData[1] == binding[1] && midiMessageData[2] == binding[2]) {
